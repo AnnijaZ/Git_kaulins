4d0c1259491260f8631194e15cd7726c8e6a51ba
Zorgenberga <annija.zorgenberga@gmail.com>
2022-01-18 11:10:48 +0200

    Izmainits ar scanner

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,12 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Kaulins {
-
+	
 	public static void main(String[] args) {
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Cik reizes mest kaulinu?");
+		int reizes = scan.nextInt();
 		int skaitlis;
+		for(int i=1; i<=reizes; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita: "+skaitlis);
+		}
+		scan.close();
 	}
 
 }
