c503294dd9a6eb954d3bc4ead8b2d54e902b9b1b
Zorgenberga <annija.zorgenberga@gmail.com>
2022-01-18 10:44:22 +0200

    Papildinats ar ciklu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class Kaulins {
 
 	public static void main(String[] args) {
-		int skaitlis;
 		Random rand = new Random();
+		int skaitlis;
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita: "+skaitlis);
 	}
